3b341357abf6eb239a02af66c8421e9317c1275a
Oscar90210 <97781025+OscarJava90210@users.noreply.github.com>
2022-06-09 15:39:36 +0500

    Delete security directory

--- security/src/main/java/com/jwtaccess/security/repo/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.jwtaccess.security.repo;
-
-import com.jwtaccess.security.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
